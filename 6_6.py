8d746df05ef4b381ecf4db98474c354a7f4ac238
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-03-30 13:20:14 +0700

    Update 6_6.py

--- 6_6.py
+++ 6_6.py
@@ -1,9 +1,9 @@
 n, k, m = map(int, input('Введите количество малышей, детей на карусели, минут: ').split())
 
-total_rides = 2*n
-times_count = total_rides//k
+all_rides = 2*n
+times_count = all_rides//k
 
-if total_rides % k != 0:
+if all_rides % k != 0:
     times_count += 1
 
 print(times_count*m)
