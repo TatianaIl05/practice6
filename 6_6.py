a443903343460af06e954624fb32aa694fffdb82
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-03-30 12:46:59 +0700

    Update 6_6.py

--- 6_6.py
+++ 6_6.py
@@ -2,7 +2,9 @@ n, k, m = map(int, input('Введите количество малышей, д
 
 whole_time = (n//k)*2*m
 
-if n % k == 0:
+if n < k:
+    print(m*2)
+elif n % k == 0:
     print(whole_time)
 else:
-    print(whole_time + (n % k) * 2 * m)
+    print(whole_time + 2 * m)
